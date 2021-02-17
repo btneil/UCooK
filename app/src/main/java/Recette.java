f19b3248a6e8e599c36f02d7220930e345a8953c
QuentinAug <quentin.augereau@epfedu.fr>
2021-02-17 17:14:16 +0100

    blablbableu

---
+++ app/src/main/java/Recette.java
@@ -0,0 +1,14 @@
+public class Recette {
+    String Nom, Instructions;
+    int Temps, Note, Difficulté, NbPersones;
+
+    public Recette(String nom, String instruction, int temps, int note, int difficulté, int nbpersonnes){
+        Nom=nom;
+        Instructions=instruction;
+        Temps=temps;
+        Note=note;
+        Difficulté=difficulté;
+        NbPersones=nbpersonnes;
+    }
+
+}
