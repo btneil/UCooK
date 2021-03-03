99fa29791fa6c5c46de5bd7babd72540ab5e2d07
maldo <maldonado.ap52@gmail.com>
2021-03-03 16:54:12 +0100

    BXL ZOO GANG

---
+++ app/src/main/java/com/example/ucook/Composition.java
@@ -0,0 +1,15 @@
+package com.example.ucook;
+
+import java.util.ArrayList;
+
+public class Composition {
+ArrayList<Ingrédient> TabIngredient = new ArrayList<Ingrédient>();
+ArrayList<String> TabQuantite = new ArrayList<String>();
+
+public Composition(){
+
+
+}
+
+
+}
