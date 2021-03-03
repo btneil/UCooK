2041202e96662756e64f45b9148b65165a08eca8
QuentinAug <quentin.augereau@epfedu.fr>
2021-03-03 17:45:14 +0100

    modif compo

--- app/src/main/java/com/example/ucook/Composition.java
+++ app/src/main/java/com/example/ucook/Composition.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 public class Composition {
     ArrayList<Ingredient> TabIngredient = new ArrayList<Ingredient>();
     ArrayList<Integer> TabQuantite = new ArrayList<Integer>();
-    //bxl zoo gang la meilleure cocaine
+
     public Composition(Ingredient ti,Integer tq){
 
     }
