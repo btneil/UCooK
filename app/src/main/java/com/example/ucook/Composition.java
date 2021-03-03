b948e8a4be3695a2b8e28009df396dc75d288679
maldo <maldonado.ap52@gmail.com>
2021-03-03 17:25:43 +0100

    BXL ZOO GANG 2

--- app/src/main/java/com/example/ucook/Composition.java
+++ app/src/main/java/com/example/ucook/Composition.java
@@ -3,9 +3,15 @@ package com.example.ucook;
 import java.util.ArrayList;
 
 public class Composition {
+    Recette rct;
+    Ingredient ignd;
+    int quantite;
     //bxl zoo gang la meilleure cocaine
-    public Composition(){
-//lol
+    public Composition(Recette r, Ingredient i, int q){
+        rct = r;
+        ignd = i;
+        quantite = q;
+        //lol
 
 }
 
