cbec5e961bc9e2fbcdc7b4138adcd45172fd6010
QuentinAug <quentin.augereau@epfedu.fr>
2021-03-03 18:02:34 +0100

    modif Recette

--- app/src/main/java/com/example/ucook/Recette.java
+++ app/src/main/java/com/example/ucook/Recette.java
@@ -1,16 +1,21 @@
 package com.example.ucook;
 
+import java.util.ArrayList;
+
 public class Recette {
     String Nom, Instructions;
-    int Temps, Note, Difficulté, NbPersones;
+    int Temps, Note, Difficulte, NbPersones;
+    ArrayList<Composition> TabComposition = new ArrayList<Composition>();
 
-    public Recette(String nom, String instruction, int temps, int note, int difficulté, int nbpersonnes){
+    public Recette(String nom, String instruction, int temps, int note, int difficulte, int nbpersonnes){
         Nom=nom;
         Instructions=instruction;
         Temps=temps;
         Note=note;
-        Difficulté=difficulté;
+        Difficulte=difficulte;
         NbPersones=nbpersonnes;
     }
+     public boolean ImporterRecette(){
 
+     }
 }
