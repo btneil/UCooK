9839a877d4efb014e0f26c3204f391c209df8a07
maldo <maldonado.ap52@gmail.com>
2021-03-03 17:35:30 +0100

    LA MEILLEURE COCAINE

--- app/src/main/java/com/example/ucook/Composition.java
+++ app/src/main/java/com/example/ucook/Composition.java
@@ -3,16 +3,10 @@ package com.example.ucook;
 import java.util.ArrayList;
 
 public class Composition {
-    Recette rct;
-    Ingredient ignd;
-    int quantite;
+    ArrayList<Ingredient> TabIngredient = new ArrayList<Ingredient>();
+    ArrayList<String> TabQuantite = new ArrayList<String>();
     //bxl zoo gang la meilleure cocaine
-    public Composition(Recette r, Ingredient i, int q){
-        rct = r;
-        ignd = i;
-        quantite = q;
-
-
+    public Composition(ArrayList ti,ArrayList tq){
 }
 
 
