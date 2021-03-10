33b20da1cb4824abee1d255c47abf40ff2b351f7
maldo <maldonado.ap52@gmail.com>
2021-03-10 10:48:57 +0100

    je sais pas

--- app/src/main/java/com/example/ucook/ListeDeCourse.java
+++ app/src/main/java/com/example/ucook/ListeDeCourse.java
@@ -6,33 +6,27 @@ public class ListeDeCourse {
     ArrayList<Composition> TabCompo = new ArrayList<Composition>();
 
 
+    public void MettreDansLePanier(Recette recette) {
+        //while (recette.TabComposition.hasNext());
+        for (int i = 0; i < recette.TabComposition.size(); i++) {
+            Ingredient ingcourant = recette.TabComposition.get(i).Ingredient;
+            int qttcourant = recette.TabComposition.get(i).Quantite;
+            for (int j = 0; j < TabCompo.size(); j++) {
+                if (ingcourant == TabCompo.get(j).Ingredient) {
+                    int qttfinal = qttcourant + TabCompo.get(j).Quantite;
+                    TabCompo.remove(j);
+                    Composition compofinale = new Composition(ingcourant, qttfinal);
+                    TabCompo.add(compofinale);
 
+                } else {
+                    TabCompo.add(recette.TabComposition.get(i));
 
-
-
-}
-
-public void MettreDansLePanier(Recette recette){
-    //while (recette.TabComposition.hasNext());
-    for(int i=0;i<recette.TabComposition.size();i++){
-        Ingredient ingcourant=recette.TabComposition.get(i).Ingredient;
-        int qttcourant =recette.TabComposition.get(i).Quantite;
-        for(int j=0;j<TabCompo.size();j++){
-            if (ingcourant==TabCompo.get(j).Ingredient){
-                int qttfinal=qttcourant+TabCompo.get(j).Quantite;
-                TabCompo.remove(j);
-                Composition compofinale=new Composition(ingcourant,qttfinal);
-                TabCompo.add(compofinale);
-
+                }
             }
-            else {
-                TabCompo.add(recette.TabComposition.get(i));
 
-            }
+
         }
 
 
     }
-
-
 }
