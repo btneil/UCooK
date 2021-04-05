959d4be8fa7b580f9e5e9303fede6232e093cab2
QuentinAug <quentin.augereau@epfedu.fr>
2021-04-05 02:18:49 +0200

    ESSAI BDD SQL

--- app/src/main/java/com/example/ucook/MenuPrincipal.java
+++ app/src/main/java/com/example/ucook/MenuPrincipal.java
@@ -4,28 +4,40 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
 
 public class MenuPrincipal extends AppCompatActivity {
 
     RecyclerView recyclerView;
+    FloatingActionButton Ajouter_rct_btn;
+
+    MyDataBase myDB;
+    ArrayList<String> rct_id, rct_titre, rct_diff, rct_ing, rct_tmps, rct_inst;
 
     String s1[], s2[];
-    int images[] = {R.drawable.banane_plantain};
-    //,R.drawable.daube_carotte,
-    //            R.drawable.galette_des_rois,R.drawable.gateau_choco,R.drawable.gnocci,
-    //            R.drawable.pdt_hasselback,R.drawable.poivron_farci,R.drawable.pokebowl,
-    //            R.drawable.salade_nicoise,R.drawable.tomate_provencale,R.drawable.lasagne,
-    //            R.drawable.lasagne,R.drawable.carotte,R.drawable.lasagne,
-    //            R.drawable.carotte,R.drawable.lasagne,R.drawable.carotte,
-    //            R.drawable.lasagne,R.drawable.carotte,R.drawable.lasagne,
-    //            R.drawable.carotte,R.drawable.lasagne,R.drawable.carotte,
-    //            R.drawable.lasagne,R.drawable.carotte,R.drawable.lasagne,
-    //            R.drawable.carotte,R.drawable.lasagne,R.drawable.carotte,
-    //            R.drawable.lasagne,R.drawable.carotte,R.drawable.lasagne
+    int images[] = {R.drawable.banane_plantain,R.drawable.daube_carotte,
+                R.drawable.galette_des_rois,R.drawable.gateau_choco,R.drawable.gnocci,
+                R.drawable.pdt_hasselback,R.drawable.poivron_farci,R.drawable.pokebowl,
+                R.drawable.salade_nicoise,R.drawable.tomate_provencale,R.drawable.lasagne,
+                R.drawable.lasagne,R.drawable.carotte,R.drawable.lasagne,
+                R.drawable.carotte,R.drawable.lasagne,R.drawable.carotte,
+                R.drawable.lasagne,R.drawable.carotte,R.drawable.lasagne,
+                R.drawable.carotte,R.drawable.lasagne,R.drawable.carotte,
+                R.drawable.lasagne,R.drawable.carotte,R.drawable.lasagne,
+                R.drawable.carotte,R.drawable.lasagne,R.drawable.carotte,
+                R.drawable.lasagne,R.drawable.carotte,R.drawable.lasagne};
 
     Recette CroqueM=new Recette();
     String [] Tab_recette = {CroqueM.Nom};
+    String[] Tab_diff = {CroqueM.Difficulte};
 
 
     @Override
@@ -34,14 +46,51 @@ public class MenuPrincipal extends AppCompatActivity {
         setContentView(R.layout.activity_menu_principal);
 
         recyclerView = findViewById(R.id.recyclerView);
+        Ajouter_rct_btn = findViewById(R.id.Ajouter_rct_btn);
+        Ajouter_rct_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MenuPrincipal.this, AddActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        myDB = new MyDataBase(MenuPrincipal.this);
+
 
         s1 = getResources().getStringArray(R.array.titre_liste_recette); //entrer ici les titres des recettes
-        //s2 = getResources().getStringArray(R.array.description ); //entrer ici les descriptions des recettes (note: pour l'instant présents dans le fichier STRING dans l'ordre)
-        s2 = Tab_recette;
+        s2 = getResources().getStringArray(R.array.description ); //entrer ici les descriptions des recettes (note: pour l'instant présents dans le fichier STRING dans l'ordre)
+        //s1 = Tab_recette;
+        //s2 = Tab_diff;
 
         MyAdapter myAdapter = new MyAdapter(this,s1,s2,images);
         recyclerView.setAdapter(myAdapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        /*rct_id = new ArrayList<>();
+        rct_titre = new ArrayList<>();
+        rct_diff = new ArrayList<>();
+        rct_ing = new ArrayList<>();
+        rct_tmps = new ArrayList<>();
+        rct_inst = new ArrayList<>();
+
+        displayData();*/
 
     }
+
+    /*void displayData(){
+        Cursor cursor = myDB.readAllData();
+        if(cursor.getCount() == 0){
+            Toast.makeText(this, "Pas de Recette", Toast.LENGTH_SHORT).show();
+        }
+        else{
+            while(cursor.moveToNext()){
+                rct_id.add(cursor.getString(0));
+                rct_titre.add(cursor.getString(1));
+                rct_diff.add(cursor.getString(2));
+                rct_ing.add(cursor.getString(3));
+                rct_tmps.add(cursor.getString(4));
+                rct_inst.add(cursor.getString(5));
+            }
+        }
+    }*/
 }
---
+++ app/src/main/java/com/example/ucook/MyDataBase.java
@@ -0,0 +1,79 @@
+package com.example.ucook;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+
+public class MyDataBase extends SQLiteOpenHelper {
+
+    private Context context;
+    private static final String DATA_BASE_NAME ="Liste_de_recettes";
+    private static final int DATA_BASE_VERSION =1;
+
+    private static final String TABLE_NAME = "mon_livre";
+    private static final String COLUMN_ID = "_id";
+    private static final String COLUMN_TITLE = "titre_rct";
+    private static final String COLUMN_DIFF = "difficulte";
+    private static final String COLUMN_ING = "ingredients";
+    private static final String COLUMN_TEMPS = "temps";
+    private static final String COLUMN_INST = "instructions";
+
+    public MyDataBase(@Nullable Context context) {
+        super(context, DATA_BASE_NAME, null, DATA_BASE_VERSION);
+        this.context = context;
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String query = "CREATE TABLE " + TABLE_NAME +
+                        " (" + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                        COLUMN_TITLE + " TEXT, " +
+                        COLUMN_DIFF + " TEXT, " +
+                        COLUMN_ING + " TEXT, " +
+                        COLUMN_TEMPS + " int, " +
+                        COLUMN_INST + " TEXT);"; //Declaration du SQL dans query, ATTENTION AUX ESPACES ! ! !
+
+        db.execSQL(query);
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int i, int i1) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
+    }
+
+    void ajouter_rct(String titre, String diff, String Ing, int tmps, String instructions){
+        SQLiteDatabase db = this.getWritableDatabase(); //pour écrire das la bdd
+        ContentValues cv = new ContentValues(); //ce qui permet d'écrire dans la bdd
+
+        cv.put(COLUMN_TITLE, titre);
+        cv.put(COLUMN_DIFF, diff);
+        cv.put(COLUMN_ING, Ing);
+        cv.put(COLUMN_TEMPS, tmps);
+        cv.put(COLUMN_INST, instructions);
+        long result = db.insert(TABLE_NAME,null, cv);
+        if (result==-1){ // si l'appli échoue
+            Toast.makeText(context, "ECHEC DE LA BDD", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            Toast.makeText(context, "SUCCES BDD", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    Cursor readAllData(){
+        String query = "SELECT * FROM " + TABLE_NAME;
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        Cursor cursor = null;
+        if(db!=null){
+            cursor = db.rawQuery(query, null);
+        }
+        return cursor; //cursor contient donc mtn toute la bdd
+    }
+}
