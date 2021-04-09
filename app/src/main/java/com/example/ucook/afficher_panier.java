55302c58427f41722c2bf9aae7e0dcd519481558
btneil <neil.batouche@epfedu.fr>
2021-04-09 16:12:20 +0200

    creation panier

--- app/src/main/java/com/example/ucook/afficher_panier.java
+++ app/src/main/java/com/example/ucook/afficher_panier.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class AfficherPanier extends AppCompatActivity {
+public class afficher_panier extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
